cd0e648bffac508f574ba541e3c0784657596024
ahmed buhamad <Ahmed.buhamad@live.com>
2015-05-10 18:54:02 +0300

    user

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -39,6 +39,6 @@ Rails.application.configure do
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
 
   # Required for Heroku
- config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
- 
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+
 end
